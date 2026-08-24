d65ad5d609436103592b16c2fac9b30619901a9c
Temmy2026 <kamarudeenabubakar87@gmail.com>
2026-08-24 10:21:03 +0100

    chore: setup storybook

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,7 +1,11 @@
+// For more info, see https://github.com/storybookjs/eslint-plugin-storybook#configuration-flat-config-format
+import storybook from "eslint-plugin-storybook";
+
 import eslint from '@eslint/js';
 import tseslint from 'typescript-eslint';
 
 export default tseslint.config(
   eslint.configs.recommended,
   ...tseslint.configs.recommended,
+  storybook.configs["flat/recommended"]
 );
